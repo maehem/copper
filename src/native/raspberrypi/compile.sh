a9098026b13003897b1ccaca34782e4d3b171247
Mark J Koch <maehem@users.noreply.github.com>
2022-09-17 14:27:19 -0700

    Correct path to Java_HOME

--- src/native/raspberrypi/compile.sh
+++ src/native/raspberrypi/compile.sh
@@ -1,5 +1,5 @@
  #    compile on Pi:
-JAVA_HOME=/usr/lib/java-17-openjdk-arm64
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64/
  g++ -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux com_maehem_copper_pi_NativeControllerImpl.cpp -o libcopper.o   
  g++ -shared -fPIC -o lib4copper.so libcopper.o -lc
 
