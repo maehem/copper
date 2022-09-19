05806fd7c55c1d9ee62bd29a298e80915714dd5b
Mark J Koch <maehem@users.noreply.github.com>
2022-09-19 12:26:59 -0700

    Update compile.sh

--- src/native/raspberrypi/compile.sh
+++ src/native/raspberrypi/compile.sh
@@ -1,5 +1,9 @@
- #    compile on Pi:
+#    compile on Pi:
+# Adjust this to reflect the Java you are using.
 JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64/
- g++ -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux com_maehem_copper_pi_NativeControllerImpl.cpp -o libcopper.o   
- g++ -shared -fPIC -o libcopper.so libcopper.o -lc -lpigpio
 
+g++ -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux com_maehem_copper_pi_NativeControllerImpl.cpp -o libcopper.o   
+g++ -shared -fPIC -o libcopper.so libcopper.o -lc -lpigpio
+
+echo "If the libcopper.so file was generated without errors, then copy it to /usr/lib:"
+echo "    sudo cp libcopper.so /usr/lib"
